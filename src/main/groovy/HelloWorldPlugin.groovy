f3fc22a294901c96fd751468177a7f8050948196
David Byron <david.byron@locationlabs.com>
2016-07-21 09:28:18 -0700

    this makes ./gradlew build fail for me

--- src/main/groovy/HelloWorldPlugin.groovy
+++ src/main/groovy/HelloWorldPlugin.groovy
@@ -4,11 +4,17 @@ import org.gradle.api.tasks.GradleBuild
  
 class HelloWorldPlugin implements Plugin<Project> {     
     void apply(Project project) {     
+        project.task('doWork') {
+            doLast {
+                println 'Hello world!'
+            }
+        }
         def externalBuildFile = new File(project.getProperty('externalBuildFile'))
         
         project.task('invokeExternalBuild', type: GradleBuild) {
-            buildFile = externalBuildFile
-            tasks = ['helloWorld']
+            //buildFile = externalBuildFile
+            //tasks = ['helloWorld']
+            tasks = ['doWork']
         }
     }
 }
